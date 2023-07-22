d7fe073bafbd065b05cf5e58a2c478f4c0ea1b46
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-22 23:35:05 +0530

    Update user.py

--- user.py
+++ user.py
@@ -2,13 +2,7 @@
 import os
 from pyrogram import Client
 from pyrogram.enums import ParseMode
-
-if os.environ.get("ENV", False):
-    from sample_config import Config
-    from sample_config import LOGGER
-else:
-    from config import Config
-    from config import LOGGER
+from config import Config, LOGGER
 
 class User(Client):
     def __init__(self):
