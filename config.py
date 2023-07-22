8760f33e35679e6074cf2e3135f2ba0acc8c28b8
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-23 05:00:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os
+from os import environ
 import logging
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(os.environ.get("APP_ID", "21288218"))
-    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
-    FILE_CAPTION = os.environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-    APPROVAL_CNL = -1001592628992
+    TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
+    APP_ID = int(environ.get("APP_ID", "21288218"))
+    API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+    TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
+    FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
+    ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
     
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
