65cedc1421762ed7f4ec06146312f8448669d1d1
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-22 23:35:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,7 @@ from pyrogram import Client
 from presets import Presets as Msg
 from pyrogram.enums import ParseMode
 
-
-if bool(os.environ.get("ENV", False)):
-    from sample_config import Config
-    from sample_config import LOGGER
-else:
-    from config import Config
-    from config import LOGGER
+from config import Config, LOGGER
 
 
 class Bot(Client):
