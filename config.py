b4730d67d182dafcc2266ba06a49050b7068404e
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-23 03:39:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import logging
 
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "21288218"))
+    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
     FILE_CAPTION = os.environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     APPROVAL_CNL = -1001592628992
