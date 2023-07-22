0714aebdd9337ea56c8fca122cc1407f522e7363
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-23 01:52:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ class Config(object):
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
     FILE_CAPTION = os.environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     APPROVAL_CNL = -1001592628992
+    
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
