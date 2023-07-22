24deb308927223c6502bc4fb4616ccfb0787d0c6
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-23 02:49:59 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -177,7 +177,7 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
             current += 1
             fetched += 1
             if current % 20 == 0:
-                await msg.edit_text(text=f'''Forward Processing...\n\nTotal Messages: <code>{lst_msg_id}</code>\nCompleted Messages: <code>{current} / {lst_msg_id}</code>\nForwarded Files: <code>{forwarded}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nUnsupported Files Skipped: <code>{unsupported}</code>\n\n send "<code>cancel</code>" for stop''' 
+                await msg.edit_text(text=f'''Forward Processing...\n\nTotal Messages: <code>{lst_msg_id}</code>\nCompleted Messages: <code>{current} / {lst_msg_id}</code>\nForwarded Files: <code>{forwarded}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nUnsupported Files Skipped: <code>{unsupported}</code>\n\n send "<code>cancel</code>" for stop''') 
             if message.empty:
                 deleted += 1
                 continue
