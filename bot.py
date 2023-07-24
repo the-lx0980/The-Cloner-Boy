b4bc5cf56164682f1ef415cc177c4b0e90eb53da
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-24 21:40:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class Bot(Client):
             name="bot_session",
             api_hash=Config.API_HASH,
             api_id=Config.APP_ID,
-            session_string=Config.TG_USER_SESSION,
+            bot_token=Config.TG_BOT_TOKEN,
             sleep_threshold=30,
             workers=8,
             plugins={
