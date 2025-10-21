6da496893e95f5b6da9e0088f47d4f399c267b88
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2025-10-21 21:12:43 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -20,7 +20,7 @@ async def forwards_messages(bot, message, from_chat, to_chat, ai_caption):
                 caption = None
 
             if ai_caption and caption:
-                caption = await extract_caption_parser(caption)
+                caption = await extract_caption(caption)
 
             try:
                 await bot.send_cached_media(
