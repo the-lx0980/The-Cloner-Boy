1620f8a066e2b68545605a1a5846ff0c5047ab8f
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2025-10-21 20:51:38 +0530

    Update and rename utils.py to utils/utils.py

---
+++ utils/utils.py
@@ -0,0 +1,46 @@
+import asyncio
+from pyrogram.errors import FloodWait
+from pyrogram import enums
+import logging
+import os
+
+async def forwards_messages(bot, message, from_chat, to_chat, ai_caption):
+    if message.media: 
+        media_type = message.media.value
+        media = getattr(message, media_type, None)
+        if media:
+            if message.caption:
+                caption = message.caption
+            elif message.video:
+                caption = message.video.file_name
+            elif message.document:
+                caption = message.document.file_name
+            elif message.audio:
+                caption = message.audio.file_name 
+            else:
+                caption = None
+
+            if ai_caption and caption:
+                caption = await extract_caption_ai(caption)
+
+            try:
+                await bot.send_cached_media(
+                    chat_id=to_chat,
+                    file_id=media.file_id,
+                    caption=f"**{caption or ''}**"
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+                await forwards_messages(bot, message, from_chat, to_chat, ai_caption)
+    else:
+        try:
+            await bot.copy_message(
+                chat_id=to_chat,
+                from_chat_id=from_chat,
+                message_id=message.id,
+                caption=f'**{message.caption or ""}**',
+                parse_mode=enums.ParseMode.MARKDOWN
+            )
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await forwards_messages(bot, message, from_chat, to_chat, ai_caption)
