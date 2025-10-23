9775d9cb523e5a17828f34e56cca6b89fce3e9a7
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-23 10:37:48 +0530

    Update __init__.py

--- utils/database/__init__.py
+++ utils/database/__init__.py
@@ -1,4 +1,3 @@
 from .database import collection
 from .get_movie_year import get_or_fetch_movie_year
 from .get_series_year import get_or_fetch_series_year
-from .get_anime_year import get_or_fetch_anime_year
