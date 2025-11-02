cb053079cbc471d7aedb5b7f461a3183e4463b1e
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-11-02 21:23:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
     FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
-    STATUS_CHAT_GROUP_ID = -1001631481154
+    STATUS_CHAT_GROUP_ID = -1003194225143
     STATUS_CHANNEL_ID = -1001631481154
     STATUS_CHANNEL_MSG_ID = 15
     
