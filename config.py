922f776601efefdd2ee2b9ab7d75d039b9d91293
lx0980 <138602811+lx0980@users.noreply.github.com>
2026-03-25 13:26:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from os import environ
 
 class Config(object):
     TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(environ.get("APP_ID", "21288218"))
-    API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+    APP_ID = int(environ.get("APP_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
     TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
     FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
     ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
