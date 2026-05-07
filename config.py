12943ed0a8790071fa4ad34882fabcef054d9983
lx0980 <138602811+lx0980@users.noreply.github.com>
2026-05-07 14:54:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from os import getenv
 
 class Config(object):
     TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "")
-    APP_ID = int(getenv("APP_ID", "21288218"))
-    API_HASH = getenv("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+    APP_ID = int(getenv("APP_ID", ""))
+    API_HASH = getenv("API_HASH", "")
     TG_USER_SESSION = getenv("TG_USER_SESSION", "")
     ADMINS = [x.strip("@ ") for x in str(getenv("ADMINS", "") or "").split(",") if x.strip("@ ")]
     
