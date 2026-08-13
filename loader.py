fbb0892bbab44d29d8c6dcc753728f5c2b4d36e2
kynoze <138602811+kynoze@users.noreply.github.com>
2026-08-13 18:02:30 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,13 +1,4 @@
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-from config import Config
+from bot import app
 
 # Instantiate the app here so anyone can safely import it
-app = Client(
-    name="ForwardBot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    parse_mode=ParseMode.HTML,
-    in_memory=True
-)
+app = app
