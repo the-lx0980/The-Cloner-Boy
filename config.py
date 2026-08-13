3352007171679aadc3ff98f31c3eb67534f1c178
kynoze <138602811+kynoze@users.noreply.github.com>
2026-08-13 16:26:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,33 @@
-from os import environ
+# config.py
 
-class Config(object):
-    TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
-    APP_ID = int(environ.get("APP_ID", "21288218"))
-    API_HASH = environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_USER_SESSION = environ.get("TG_USER_SESSION", "")
-    FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-    ADMINS = [x.strip("@ ") for x in str(environ.get("ADMINS", "") or "").split(",") if x.strip("@ ")]
-    
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.getenv("API_ID", "0"))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    DB_NAME = os.getenv("DB_NAME", "forward_bot")
+
+    # Only these user IDs can use the bot
+    ADMINS = [int(x) for x in os.getenv("ADMINS", "").split() if x.isdigit()]
+
+    DEFAULT_SETTINGS = {
+        "caption_enabled": False,
+        "caption_template": "<b>{caption}</b>",
+        "replace_enabled": False,
+        "replacements": [],
+        "block_words": [],
+        "whitelist": [],
+        "whitelist_mode": False,
+        "remove_links": False,
+        "inline_buttons": [],
+        "media_types": ["photo", "video", "document", "audio", "sticker"],
+        "forward_tag": False,
+        "delay": 1.0,
+        "anti_duplicate": True
+    }
