cece312cf4eefaeaf5be2370ed4323709b67fff5
kynoze <138602811+kynoze@users.noreply.github.com>
2026-08-13 19:27:24 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 # handlers/__init__.py
 
-from . import target_handlers
-from . import settings_handlers
-from . import text_input_handlers
-from . import source_handler
+#from . import target_handlers
+#from . import settings_handlers
+#from . import text_input_handlers
+#from . import source_handler
